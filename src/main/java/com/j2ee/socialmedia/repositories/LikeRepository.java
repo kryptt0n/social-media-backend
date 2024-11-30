ad6354a5b779cc771bbc41e6adf6ca383af7f24f
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2024-11-29 22:22:26 -0500

    updated post service

--- src/main/java/com/j2ee/socialmedia/repositories/LikeRepository.java
+++ src/main/java/com/j2ee/socialmedia/repositories/LikeRepository.java
@@ -1,9 +1,15 @@
 package com.j2ee.socialmedia.repositories;
 
 import com.j2ee.socialmedia.entities.Like;
+import com.j2ee.socialmedia.entities.Post;
+import com.j2ee.socialmedia.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Integer> {
+    List<Like> findAllByUser(User user);
+    List<Like> findAllByPost(Post post);
 }
--- src/main/java/com/j2ee/socialmedia/services/PostService.java
+++ src/main/java/com/j2ee/socialmedia/services/PostService.java
@@ -1,8 +1,10 @@
 package com.j2ee.socialmedia.services;
 
 import com.j2ee.socialmedia.dto.PostDTO;
+import com.j2ee.socialmedia.entities.Like;
 import com.j2ee.socialmedia.entities.Post;
 import com.j2ee.socialmedia.entities.User;
+import com.j2ee.socialmedia.repositories.LikeRepository;
 import com.j2ee.socialmedia.repositories.PostRepository;
 import com.j2ee.socialmedia.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +16,25 @@ import java.util.Optional;
 @Service
 public class PostService {
 
+    private final LikeRepository likeRepository;
     private PostRepository postRepository;
     private UserRepository userRepository;
 
     @Autowired
-    public PostService(PostRepository postRepository, UserRepository userRepository) {
+    public PostService(PostRepository postRepository, UserRepository userRepository, LikeRepository likeRepository) {
         this.postRepository = postRepository;
         this.userRepository = userRepository;
+        this.likeRepository = likeRepository;
     }
 
-    // TODO: add user to post
     public Post create(Post post, String username) {
-        return postRepository.save(post);
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isPresent()) {
+            post.setUser(user.get());
+            return postRepository.save(post);
+        } else {
+            return null;
+        }
     }
 
     public Optional<Post> update(Integer postId, Post post) {
@@ -52,6 +61,21 @@ public class PostService {
 
     public List<PostDTO> getPostsByUserId(int userId) {
         User user = userRepository.getById(userId);
-        return null;
+        List<Post> posts = postRepository.findAllByUser(user);
+        List<Like> likes = likeRepository.findAllByUser(user);
+        List<Integer> likedPostIds = likes.stream().map(like ->
+                like.getPost().getId()
+        ).toList();
+        List<PostDTO> result = posts.stream().map(post ->
+            new PostDTO(
+                    post.getId(),
+                    post.getContent(),
+                    post.getImage(),
+                    post.getUser(),
+                    likedPostIds.contains(post.getId()),
+                    likeRepository.findAllByPost(post).size()
+            )
+        ).toList();
+        return result;
     }
 }
