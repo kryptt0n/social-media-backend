3acbbf9b9ed566c993e7a0b40c207131bb5a1c5e
Desmond <desmondzkx@gmail.com>
2025-01-11 23:07:27 -0500

    change to jwt auth

--- src/main/java/com/j2ee/socialmedia/entities/User.java
+++ src/main/java/com/j2ee/socialmedia/entities/User.java
@@ -1,14 +1,17 @@
 package com.j2ee.socialmedia.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
-public class User {
-
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -136,5 +139,30 @@ public class User {
 
     public void setRoles(String roles) {
         this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
