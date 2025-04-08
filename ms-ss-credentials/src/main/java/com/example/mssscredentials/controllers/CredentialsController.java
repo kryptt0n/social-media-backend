650f4ccf65a99e9f5847b8197ff277202078b7c3
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2025-04-08 11:51:28 -0400

    fixed mapping

--- ms-ss-credentials/src/main/java/com/example/mssscredentials/controllers/CredentialsController.java
+++ ms-ss-credentials/src/main/java/com/example/mssscredentials/controllers/CredentialsController.java
@@ -2,6 +2,8 @@ package com.example.mssscredentials.controllers;
 
 import com.example.mssscredentials.dto.CredentialsDto;
 import com.example.mssscredentials.services.CredentialsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,19 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/credentials")
 public class CredentialsController {
 
+    private static final Logger log = LoggerFactory.getLogger(CredentialsController.class);
     private final CredentialsService credentialsService;
 
     public CredentialsController(CredentialsService credentialsService) {
         this.credentialsService = credentialsService;
     }
 
-    @PostMapping("/register")
+    @PostMapping("/authenticate")
     public ResponseEntity<Boolean> authenticate(@RequestBody CredentialsDto credentials) {
         return ResponseEntity.ok(credentialsService.authenticate(credentials.getUsername(), credentials.getPassword()));
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody CredentialsDto credentials) {
+        log.warn("Attempting to register credentials with username {}", credentials.getUsername());
         credentialsService.register(credentials.getUsername(), credentials.getPassword());
         return ResponseEntity.ok().build();
     }
