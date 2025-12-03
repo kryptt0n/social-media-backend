c9fd7b1a9acc1ddf5533a46f9331a9e452d4d4d1
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2025-12-03 13:00:00 -0500

    updated dtos

--- ms-ss-credentials/src/main/java/com/example/mssscredentials/dto/UsernameResponse.java
+++
@@ -1,5 +0,0 @@
-package com.example.mssscredentials.dto;
-
-
-public record UsernameResponse(String username) {}
-
--- ms-ss-credentials/src/main/java/com/example/mssscredentials/feign/UserCrudClient.java
+++ ms-ss-credentials/src/main/java/com/example/mssscredentials/feign/UserCrudClient.java
@@ -1,12 +1,24 @@
 package com.example.mssscredentials.feign;
 
-import com.example.mssscredentials.dto.UserEmailDTO;
+import com.example.mssscredentials.dto.UserShortDTO;
+import com.example.mssscredentials.dto.UserProfileDTO;
+import com.example.mssscredentials.dto.UserProfileRegisterDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "ms-ss-user-profile-crud")
+import java.util.Optional;
+
+@FeignClient(name = "ms-ss-user-profile-crud", dismiss404 = true, path = "/usercrud")
 public interface UserCrudClient {
-    @GetMapping("/usercrud/emails/{email}")
-    UserEmailDTO getUserByEmail(@PathVariable("email") String email);
+    @GetMapping("/emails/{email}")
+    Optional<UserShortDTO> getUserByEmail(@PathVariable("email") String email);
+
+    @GetMapping("/usernames/{username}")
+    Optional<UserShortDTO> getUserByUsername(@PathVariable("username") String username);
+
+    @PostMapping("/register")
+    UserProfileDTO register(@RequestBody UserProfileRegisterDTO user);
+
+    @DeleteMapping("/delete/email/{email}")
+    void deleteUserWithEmail(@PathVariable String email);
 }
