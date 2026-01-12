62b634ca2f02f069ebd7b683850275700af45648
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2026-01-12 13:00:00 -0500

    added oauth

---
+++ ms-ss-credentials/src/main/java/com/example/mssscredentials/dto/MediaPayload.java
@@ -0,0 +1,15 @@
+package com.example.mssscredentials.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MediaPayload {
+    private String sourceId;
+    private String image;
+    private Provider provider;
+    private ImageType type;
+}
