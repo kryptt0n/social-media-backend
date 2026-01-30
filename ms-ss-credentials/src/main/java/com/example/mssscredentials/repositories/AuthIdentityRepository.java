4fb8e65058139d2999436400e33af9b65415ceba
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2026-01-30 13:00:00 -0500

    added oauth

---
+++ ms-ss-credentials/src/main/java/com/example/mssscredentials/repositories/AuthIdentityRepository.java
@@ -0,0 +1,14 @@
+package com.example.mssscredentials.repositories;
+
+import com.example.mssscredentials.dto.AuthMethodType;
+import com.example.mssscredentials.entity.AuthIdentity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuthIdentityRepository extends JpaRepository<AuthIdentity, Long> {
+    List<AuthIdentity> findAllByTypeAndValue(AuthMethodType type, String value);
+    List<AuthIdentity> findAllByUserId(Integer userId);
+}
